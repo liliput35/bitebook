fbe2796f4561c1614b785d8466a8d1c7a0ac31e4
Lorenz Ciocon <lorenzciocon@gmail.com>
2026-04-02 23:49:46 +0800

    Add: dashboard ui mobile

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,16 +4,71 @@
 
 @section('content')
 
+<div class="bg-dark-green py-4 flex justify-center md:hidden">
+    <img src="{{asset('images/logo.png')}}" alt="" class="max-w-[85px]">
+</div>
 
-<h1 class="text-2xl font-bold">Admin Dashboard</h1>
+<div class="w-[90%] mx-auto">
 
-<form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-md">
-        Logout
-    </button>
-</form>
+    <h1 class="text-[2em] font-bold mt-6">Welcome back, Chef</h1>
+    <p class="text-dark-gray text-lg leading-none">Here is what's simmering in your kitchen today.</p>
 
-@include('layouts.navbar') 
+    <div class="bg-white shadow-lg p-4 rounded-lg mt-4">
+        <div class="mb-6 w-full flex items-center gap-2">
+            <img src="{{asset('images/revenue-icon.png')}}" alt="" class="mw-[25px]">
+            <div class="w-full flex justify-between">
+                <div class="">
+                    <h4 class="text-light-gray font-medium leading-none">TOTAL REVENUE</h4>
+                    <p class="font-medium text-[1.5em]">P0000.00</p>
+                </div>
+
+                <p class="font-medium text-light-green">+12%</p>
+            </div>
+        </div>
+        <div class="mb-6 w-full flex items-center gap-2">
+            <img src="{{asset('images/inquiries-icon.png')}}" alt="" class="mw-[25px]">
+            <div class="w-full flex justify-between">
+                <div class="">
+                    <h4 class="text-light-gray font-medium leading-none">TOTAL REVENUE</h4>
+                    <p class="font-medium text-[1.5em]">P0000.00</p>
+                </div>
+
+                <p class="font-medium text-light-green">+12%</p>
+            </div>
+        </div>
+        <div class="mb-6 w-full flex items-center gap-2">
+            <img src="{{asset('images/bookings-icon.png')}}" alt="" class="mw-[25px]">
+            <div class="w-full flex justify-between">
+                <div class="">
+                    <h4 class="text-light-gray font-medium leading-none">TOTAL REVENUE</h4>
+                    <p class="font-medium text-[1.5em]">P0000.00</p>
+                </div>
+
+                <p class="font-medium text-light-green">+12%</p>
+            </div>
+        </div>
+        <div class="mb-6 w-full flex items-center gap-2">
+            <img src="{{asset('images/popular-icon.png')}}" alt="" class="mw-[25px]">
+            <div class="w-full flex justify-between">
+                <div class="">
+                    <h4 class="text-light-gray font-medium leading-none">TOTAL REVENUE</h4>
+                    <p class="font-medium text-[1.5em]">P0000.00</p>
+                </div>
+
+                <p class="font-medium text-light-green">+12%</p>
+            </div>
+        </div>
+
+    </div>
+
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-md mt-8">
+            Logout
+        </button>
+    </form>
+</div>
+
+@include('layouts.admin_navbar') 
 
 @endsection
--- resources/views/layouts/admin_navbar.blade.php
+++ resources/views/layouts/admin_navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="navbar">
+<div class="navbar bg-dark-green text-white flex items-center justify-between w-[90%] py-2 px-4 rounded-full fixed bottom-[1em] left-0 right-0 mx-auto">
     <a href="{{ route('admin.dashboard') }}">Dashboard</a>
     <a href="{{ route('admin.menu') }}">Menu</a>
     <a href="{{ route('admin.inquiries') }}">Inquiry</a>
