96ea1823998cd3520299a3fd8ca049d5ea8b9eb2
Lorenz Ciocon <lorenzciocon@gmail.com>
2026-04-27 13:23:57 +0800

    Update: user navbar fixes

--- resources/views/layouts/user_navbar.blade.php
+++ resources/views/layouts/user_navbar.blade.php
@@ -6,15 +6,14 @@
     <div class="flex justify-between items-center font-medium text-dark-gray text-[1.25em] gap-[2.5em]">
         <a href="{{ route('user.menu') }}">Menu</a>
         <a href="{{ route('user.bundles') }}">Bundles</a>
-        <a href="{{ route('user.book') }}">Book</a> 
         <a href="{{ route('user.inquiries') }}">Inquiries</a> 
-        <a href="{{ route('user.cart') }}">Cart</a> 
+        <a href="{{ route('user.bookings') }}">Bookings</a> 
 
     </div> 
 
     <div class="flex items-center gap-3">
+        <a href="{{ route('user.cart') }}"><img src="{{asset('images/shopping-cart.png')}}" alt="" class="w-[40px]"></a>
         <a href="{{ route('user.profile') }}"><img src="{{asset('images/profile-icon.png')}}" alt=""></a>
-        <a href="{{ route('logout') }}"><img src="{{asset('images/logout-icon.png')}}" alt="" class="w-[40px]"></a>
 
     </div>
 
@@ -25,7 +24,7 @@
 <div class="navbar bg-dark-green text-white flex items-center justify-between w-[90%] py-2 px-4 rounded-full fixed bottom-[1em] left-0 right-0 mx-auto lg:hidden">
     <a href="{{ route('user.menu') }}">Menu</a>
     <a href="{{ route('user.bundles') }}">Bundles</a>
-    <a href="{{ route('user.book') }}">Book</a>
     <a href="{{ route('user.inquiries') }}">Inquiries</a>
+    <a href="{{ route('user.bookings') }}">Bookings</a> 
     <a href="{{ route('user.cart') }}">Cart</a> 
 </div>
